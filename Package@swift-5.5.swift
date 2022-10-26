e19af2b2856239bb31ec94ae99951c04978ddcd9
Igor Chertenkov <woolstrand@gmail.com>
2022-10-26 15:34:52 +0600

    Changed dependencies to forks

--- Package@swift-5.5.swift
+++ Package@swift-5.5.swift
@@ -21,7 +21,7 @@ let package = Package(
         .library(name: "NIOHTTP2", targets: ["NIOHTTP2"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-nio.git", from: "2.35.0"),
+        .package(url: "https://github.com/Shackle-Group/swift-nio.git", from: "2.35.0"),
         .package(url: "https://github.com/apple/swift-atomics.git", from: "1.0.2"),
     ],
     targets: [
